c5f82797ed9406aa1705ba1008903c74d4ae8b5b
Lynam-Smith <Miles.Lynam-Smith@telekom.de>
2022-06-02 14:34:16 +0200

    School repo

--- src/main/java/com/wildcodeschool/wildandwizard/repository/WizardRepository.java
+++ src/main/java/com/wildcodeschool/wildandwizard/repository/WizardRepository.java
@@ -7,7 +7,6 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
 import com.wildcodeschool.wildandwizard.entity.Wizard;
 
 import util.JdbcUtils;
